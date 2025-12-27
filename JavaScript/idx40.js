c4d33922744f3ae0a42ea6d2a22f820afd8b9f39
ankit-prabhavak <ankitabcd1718@gmail.com>
2025-12-27 20:15:05 +0530

    New Js concept

---
+++ JavaScript/idx40.js
@@ -0,0 +1,51 @@
+// var is function scope or global scope. Redeclaration is also possible
+
+// var age = 40;
+
+
+// if(true) {
+//     console.log(age);
+// }
+
+
+// function definition
+function solve() {
+    var age = 25;
+    console.log(25);
+}
+
+// Function call
+solve();
+
+
+// let keyword is block scope;
+let a = 20;
+a = 30;
+
+// let a = 30; error 
+
+
+// const a = 10;  Nothing is possible if declared once
+
+
+// Variable naming conventions
+/*
+
+
+
+*/
+
+
+// Data Types
+// Primitive and non-primitive data types
+
+/*
+Primitve DataTypes
+Boolean
+Number 
+String
+Symbol
+BigInt
+Undefined
+NULL
+*/
